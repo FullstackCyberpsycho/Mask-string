5b827a1f848d353893071034b4f9a725e69047ac
Denis <Denis@email.com>
2024-11-07 22:07:17 +1000

    optimization 1

--- src/main/java/org/example/MaskString.java
+++ src/main/java/org/example/MaskString.java
@@ -4,15 +4,22 @@ public class MaskString {
     private final String VOWELS = "aeiouAEIOU";
 
     public String vowelReplacement(String text) {
-        StringBuilder stringBuilder = new StringBuilder(text);
-
-        for (int i = 0; i < text.length(); i++) {
+        StringBuilder replacement = new StringBuilder(text);
+        /*for (int i = 0; i < text.length(); i++) {
             if (VOWELS.indexOf(text.charAt(i)) != -1) {
                 stringBuilder.setCharAt(i, '*');
             }
-        }
+        }*/
 
-        return stringBuilder.toString();
+        text.chars().forEach(c -> {
+            if(VOWELS.indexOf(c) != -1) {
+                replacement.append('*');
+            } else {
+                replacement.append((char) c);
+            }
+        });
+
+        return replacement.toString();
     }
 
     public static String vowelReplacement(int text) {
